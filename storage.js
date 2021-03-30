89c81d145bda39e92d187668db65b55f6c329798
Ron Marsiano <ronmarsiano@gmail.com>
2021-03-30 11:59:49 +0300

    update

--- storage.js
+++ storage.js
@@ -1,11 +1,26 @@
 fs = require('fs');
 module.exports = {
-
+    storage: undefined,
     save_templates: function(templates){
-        fs.writeFile('templates.json', JSON.stringify(templates), function (err) {
-            if (err) return console.log(err);
-            console.log('Hello World > helloworld.txt');
-          });
+        templates.snapshot_time = Date.now();
+        if(this.should_save(templates)){
+            console.log("Should save now - " + templates.snapshot_time + " snapshot - " + this.get_templates().snapshot_time+ " Diff = " +(templates.snapshot_time - this.get_templates().snapshot_time))
+            fs.writeFile('templates.json', JSON.stringify(templates), function (err) {
+                if (err) return console.log(err);
+              });
+        }
+        else{
+            console.log("Should not save now - " + templates.snapshot_time + " snapshot - " + this.get_templates().snapshot_time+ " Diff = " +(templates.snapshot_time - this.get_templates().snapshot_time))
+        }
+
+    },
+    should_save: function (new_snapshot) {
+        var time_diff = 7 * 24 * 3600 * 1000;
+        let old_snapshots = this.get_templates();
+        console.warn(!old_snapshots.snapshot_time )
+        console.warn(((new_snapshot.snapshot_time - old_snapshots.snapshot_time) > time_diff) )
+        console.warn( !old_snapshots.snapshot_time || ((new_snapshot.snapshot_time - old_snapshots.snapshot_time) > time_diff) )
+        return !old_snapshots.snapshot_time || ((new_snapshot.snapshot_time - old_snapshots.snapshot_time) > time_diff) 
     },
     get_templates: function(){
         let rawdata = fs.readFileSync('templates.json');
